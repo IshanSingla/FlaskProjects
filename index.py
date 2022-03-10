1c4cdf08d1d25e317e8fe0a99deb06ca842a9641
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-10 23:57:26 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify, request, render_template, send_file
 import firebase_admin, carbon, asyncio, os
 from firebase_admin import db,credentials
 from flask_cors import CORS
+from pyppeteer import launch
 cred = credentials.Certificate('1.json')
 default_app = firebase_admin.initialize_app( cred,{'databaseURL':"https://flask-c50a2-default-rtdb.asia-southeast1.firebasedatabase.app/"})
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.secret_key = 'i_iz_noob'
 loop = asyncio.get_event_loop()
 CORS(app)
@@ -68,6 +69,139 @@ def home():
         return send_file((os.getcwd() + '/carbon_screenshot.png'), mimetype='image/png')
     except Exception as e:
         return jsonify({"error": e})
+
+class carbon:
+    DOWNLOAD_FOLDER = os.getcwd()
+    defaultOptions = {
+        "backgroundColor": "rgba(171, 184, 195, 1)",
+        "code": "",
+        "dropShadow": True,
+        "dropShadowBlurRadius": "68px",
+        "dropShadowOffsetY": "20px",
+        "exportSize": "2x",
+        "fontFamily": "Hack",
+        "firstLineNumber": 1,
+        "fontSize": "14px",
+        "language": "auto",
+        "lineNumbers": False,
+        "paddingHorizontal": "56px",
+        "paddingVertical": "56px",
+        "squaredImage": False,
+        "theme": "seti",
+        "watermark": False,
+        "widthAdjustment": True,
+        "windowControls": True,
+        "windowTheme": None,
+    }
+
+    optionToQueryParam = {
+        "backgroundColor": "bg",
+        "code": "code",
+        "dropShadow": "ds",
+        "dropShadowBlurRadius": "dsblur",
+        "dropShadowOffsetY": "dsyoff",
+        "exportSize": "es",
+        "fontFamily": "fm",
+        "firstLineNumber": "fl",
+        "fontSize": "fs",
+        "language": "l",
+        "lineNumbers": "ln",
+        "paddingHorizontal": "ph",
+        "paddingVertical": "pv",
+        "squaredImage": "si",
+        "theme": "t",
+        "watermark": "wm",
+        "widthAdjustment": "wa",
+        "windowControls": "wc",
+        "windowTheme": "wt",
+    }
+
+    ignoredOptions = [
+        # Can't pass these as URL (So no support now)
+        "backgroundImage",
+        "backgroundImageSelection",
+        "backgroundMode",
+        "squaredImage",
+        "hiddenCharacters",
+        "name",
+        "lineHeight",
+        "loading",
+        "icon",
+        "isVisible",
+        "selectedLines",
+    ]
+
+    async def open_carbonnowsh(url):
+        browser = await launch(defaultViewPort=None,
+                            handleSIGINT=False,
+                            handleSIGTERM=False,
+                            handleSIGHUP=False,
+                            headless=True,
+                            args=['--no-sandbox', '--disable-setuid-sandbox'])
+        page = await browser.newPage()
+        await page._client.send('Page.setDownloadBehavior', {
+            'behavior': 'allow', 
+            'downloadPath': carbon.DOWNLOAD_FOLDER
+        })
+        await page.goto(url, timeout=100000)
+        return browser, page
+
+
+    async def get_response(url, path):
+        browser, page = await carbon.open_carbonnowsh(url)
+        element = await page.querySelector("#export-container  .container-bg")
+        img = await element.screenshot({'path': path})
+        await browser.close()
+        return (path)
+
+    def validateBody(body_):
+        validatedBody = {}
+        if not body_['code']:
+            raise Exception("code is required for creating carbon")
+
+        for option in body_:
+            if option in carbon.ignoredOptions:
+                print(f"Unsupported option: {option} found. Ignoring!")
+                continue
+            if (not (option in carbon.defaultOptions)):
+                continue
+                print(f"Unexpected option: {option} found. Ignoring!")
+                #raise Exception(f"Unexpected option: {option}")
+            validatedBody[option] = body_[option]
+        return validatedBody
+
+
+    def createURLString(validatedBody):
+        base_url = "https://carbon.now.sh/"
+        first = True
+        url = ""
+        try:
+            if validatedBody['backgroundColor'].startswith('#') or carbon.checkHex(validatedBody['backgroundColor'].upper()) == True:
+                validatedBody['backgroundColor'] = carbon.hex2rgb(
+                    validatedBody['backgroundColor'])
+        except KeyError:
+            pass
+        for option in validatedBody:
+            if first:
+                first = False
+                url = base_url + \
+                    f"?{carbon.optionToQueryParam[option]}={validatedBody[option]}"
+            else:
+                url = url + \
+                    f"&{carbon.optionToQueryParam[option]}={validatedBody[option]}"
+        return url
+
+
+    def hex2rgb(h):
+        h = h.lstrip('#')
+        return ('rgb'+str(tuple(int(h[i:i+2], 16) for i in (0, 2, 4))))
+
+    def checkHex(s):
+        for ch in s:
+            if ((ch < '0' or ch > '9') and (ch < 'A' or ch > 'F')):  
+                return False
+        return True
+
 
 """
 @app.route('/Gen/<string:n>')
