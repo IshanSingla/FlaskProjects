61050cffae101f0b5c20fa33544f7d2192cb06ac
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-11 00:23:16 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask, jsonify, request, render_template, send_file
 import firebase_admin, carbon, asyncio, os
 from firebase_admin import db,credentials
@@ -229,4 +230,4 @@ def Clear(n):
 """
 
 if __name__ == "__main__":
-    app.run(use_reloader=True, threaded=True)
+    app.run(debug=True,use_reloader=True, threaded=True)
