7a0db877d1a785e275c3ff385f8edbd29fa17f74
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-23 00:16:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -332,6 +332,55 @@ def moneytrans():
         return jsonify({"stats": f"{e}"})
 
 
+    
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
+
+    videos = []
+
+    if request.method == 'POST':
+        search_params = { 
+            'key' : "AIzaSyDHaYtqlyjOIljQbfRvCxHgfSB3Jtn8DSQ",
+            'q' : request.form.get('query'),
+            'part' : 'snippet',
+            'maxResults' : 9,
+            'type' : 'video'
+        }
+
+        r = requests.get(search_url, params=search_params)
+
+        results = r.json()['items']
+
+        video_ids = []
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+
+        if request.form.get('submit') == 'lucky':
+            return redirect(f'https://www.youtube.com/watch?v={ video_ids[0] }')
+
+        video_params = {
+            'key' : "AIzaSyDHaYtqlyjOIljQbfRvCxHgfSB3Jtn8DSQ",
+            'id' : ','.join(video_ids),
+            'part' : 'snippet,contentDetails',
+            'maxResults' : 9
+        }
+
+        r = requests.get(video_url, params=video_params)
+        results = r.json()['items']
+        for result in results:
+            video_data = {
+                'id' : result['id'],
+                'url' : f'https://www.youtube.com/watch?v={ result["id"] }',
+                'thumbnail' : result['snippet']['thumbnails']['high']['url'],
+                'duration' : int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
+                'title' : result['snippet']['title'],
+            }
+            videos.append(video_data)
+        
+    return render_template('yt.html', videos=videos)
+
 
 defaultOptions = {
         "backgroundColor": "rgba(171, 184, 195, 1)",
