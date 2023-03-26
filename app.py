52da4e4c83f6603a224a464175ab141b8a096f7c
IshanSingla <is.ishan.singla@gmail.com>
2023-03-26 18:42:47 +0530

    Update: First Commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from pyresparser import ResumeParser
+import os
+from flask import Flask, request, jsonify
+import nltk
+nltk.download('stopwords')
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return """
+    <body style='text-align: center;'>
+        <form method="post" action="/" enctype = "multipart/form-data">
+            <input name="file" type="file"/>
+            <button>Submit</button>
+        </form>
+    </body>
+    """
+
+
+@app.route('/', methods=['POST'])
+def parse_resume():
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file uploaded'})
+        file = request.files['file']
+        if file.filename.endswith('.pdf') == False:
+            return jsonify({'error': 'File must be a PDF'})
+        file_path = os.path.join(os.getcwd(), file.filename)
+        file.save(file_path)
+
+        data = ResumeParser(file_path).get_extracted_data()
+        os.remove(file_path)
+
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": e})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, threaded=True,
+            host='0.0.0.0', port=os.getenv('PORT', 9050))
